5d82d7c36032158800da7f244a73f417a119e192
KevinVivanco_02 <kev.vivanco@duocuc.cl>
2024-09-05 23:34:53 -0400

    Botón de cerrar sesión
    Se mantiene lo mismo del Commit anterior solo que se agregó un botón en "tab3" para regresar a "login".

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss']
 })
-export class Tab3Page {
+export class Tab3Page implements OnInit {
 
-  constructor() {}
+  constructor(private router:Router) { }
+
+  ngOnInit() {
+  }
+
+  logOut(){
+    this.router.navigate(['./login']);
+  }
 
 }
