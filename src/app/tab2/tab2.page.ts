b038ab04c3f9aa9304cb056f1677b47d2a0f7838
EllZzz <el.zampeze@duocuc.cl>
2024-09-08 23:56:36 -0300

    Consumption Summary
    calc wow

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -7,6 +7,31 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
-  constructor() {}
+  appliances = [
+    { name: 'Microondas', kWh: 0.8, costPerHour: 3, image: 'assets/images/microwave.png' },
+    { name: 'Lavadora', kWh: 9.4, costPerHour: 1441, image: 'assets/images/washing-machine.png' },
+    { name: 'Refrigerador', kWh: 7.6, costPerHour: 1794, image: 'assets/images/fridge.png' },
+  ];
+
+  currentDate = new Date().toLocaleDateString('en-GB', { day: '2-digit', month: 'short', year: 'numeric' });
+
+ // Datos resumen
+ totalKw = 0;
+ costPerMonth = 0;
+ totalCost = 0;
+
+ ngOnInit() {
+   this.calculateSummary();
+ }
+
+ calculateSummary() {
+   // Calcular el total de kWh y costo
+   this.totalKw = this.appliances.reduce((sum, appliance) => sum + appliance.kWh, 0);
+   this.costPerMonth = this.appliances.reduce((sum, appliance) => sum + appliance.costPerHour, 0);
+
+   // Calcular los costos basados en el total de kWh
+   this.costPerMonth = this.costPerMonth; // 
+   this.totalCost = this.costPerMonth;
+}
 
 }
